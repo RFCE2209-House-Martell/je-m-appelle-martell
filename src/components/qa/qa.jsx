4ee7fc3ed5ca4d5a3ebbcecca8b2d92738e85062
Halley Bennett <halley.bennett@gmail.com>
2022-10-12 15:31:44 -0600

    Completed Features files

---
+++ src/components/qa/qa.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Qa = () => {
+  return (<div>Qa</div>);
+}
+
+export default Qa;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'; import ReactDOM from 'react-dom';
+import Overview from './compontents/overview/overview.jsx';
+import Qa from './compontents/qa/qa.jsx';
+import Ratings from './compontents/ratings/ratings.jsx';
+import Related from './compontents/related/related.jsx';
 
 const App = () => {
-  return (
+  return (<div>
     <div>
       <h1>Hello World Again!</h1>
-    </div>);
+    </div>
+    <div>
+      <Overview />
+      <Related />
+      <Qa />
+      <Ratings />
+    </div>
+  </div>);
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
