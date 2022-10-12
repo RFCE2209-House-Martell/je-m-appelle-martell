425185eedd61b9a343e76845674747d81b412675
czhu67 <caitlin.zhu2010@gmail.com>
2022-10-12 14:06:02 -0700

    routes created

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
+const overviewRoute = require('./routes/overview.js');
+const qaRoute = require('./routes/qa.js');
+const ratingsRoute = require('./routes/ratings.js');
+const relatedRoute = require('./routes/related.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "../public"));
 
 app.listen(3000); console.log('Listening on port 3000');
