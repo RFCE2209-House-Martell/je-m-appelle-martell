673e2d57d8b484a61fdde69ed62b8d8a56b4f5bf
fitdizzle <markfitnessaz@gmail.com>
2022-10-13 12:23:52 -0700

    added commented example for the reusable modal

--- src/components/sharedFolder/modal.jsx
+++ src/components/sharedFolder/modal.jsx
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
+// In parent component
+//   const [show, setShow] = useState(false);
+
+//<div>
+// <button onClick={() => setShow(true)}>Click Here</button>
+//  <Modal show={show} onClose={() => setShow(false)}>
+//     "Whatever you want to render in the modal"
+// </Modal>
+// </div>
+
+
 const Modal = (props) => {
   if (!props.show) {
     return null;
@@ -17,3 +28,15 @@ const Modal = (props) => {
 };
 
 export default Modal;
+
+// const modalStyles = {
+//   position: 'fixed',
+//   left: '0',
+//   right: '0',
+//   top: '0',
+//   bottom: '0',
+//   backgroundColor: 'rgba(0,0,0,0.5)',
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center'
+// };
