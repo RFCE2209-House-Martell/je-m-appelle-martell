57b1066b71d00a4ab1c9c6983c994b648320e989
czhu67 <caitlin.zhu2010@gmail.com>
2022-10-14 08:24:28 -0700

    added global states productId and avgStars

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Overview from './components/overview/overview.jsx';
 import Qa from './components/qa/qa.jsx';
 import Ratings from './components/ratings/ratings.jsx';
 import RelatedApp from './components/related/RelatedApp.jsx';
 
-
 const App = () => {
+  const [productId, setProductId] = useState(1);
+  const [avgStars, setAvgStars] = useState(0);
+
   return (<div>
     <div>
       <h1>Hello World Again!</h1>
     </div>
     <div>
-      <Overview />
-      <RelatedApp />
-      <Qa />
-      <Ratings />
+      <Overview productId={productId} avgStars={avgStars}/>
+      <RelatedApp productId={productId} setProductId={setProductId}/>
+      <Qa productId={productId}/>
+      <Ratings productId={productId} avgStars={avgStars} setAvgStars={setAvgStars}/>
     </div>
   </div>);
 }
