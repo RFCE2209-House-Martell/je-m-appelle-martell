23854759e0823cba9b44fcfc100b776cba74dc5b
Christopher LeBlancgit config --global user.email chrisleblanc1994@gmail.com <chrisleblanc1994@gmail.com>
2022-10-14 13:05:46 -0500

    OutfitCard and AddOutfitCard skeletons created with minimal styling

--- src/components/related/AddOutfitCard.jsx
+++ src/components/related/AddOutfitCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+// import style from './styling.css'
 
 const AddOutfitCard = (props) => {
 
+  const componentStyle = {
+    height: '300px',
+    width: '200px',
+    borderStyle: 'solid',
+    borderColor: 'black'
+  }
 
-  return(
-    <div>
 
+
+  return (
+    <div style={componentStyle}>
+      <button>+</button>
     </div>
   )
 }
 
 export default AddOutfitCard;
+
+//style to a particular size
