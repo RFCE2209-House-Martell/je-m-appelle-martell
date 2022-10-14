1613de7e0389a83bad84ea9e73c8ece14639e9a6
Halley Bennett <halley.bennett@gmail.com>
2022-10-14 15:15:23 -0600

    Completed Ratings SKeleton

--- src/components/ratings/ratingsSection.jsx
+++ src/components/ratings/ratingsSection.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CharacteristicsCreator from './characteristicsCreator.jsx'
 
 const RatingsSection = (props) => {
   // console.log(props.sampleMetaData.ratings)
+  const [starsObj, setStarsObj] = useState(props.sampleMetaData.ratings)
+  var totalRatings = 0;
+  var totalStars = 0;
+
+  const findAvgStars = () => {
+    for (var key in starsObj) {
+      totalRatings = (totalRatings + starsObj[key])
+      totalStars = (totalStars + (key * starsObj[key]))
+    }
+    props.setAvgStars(totalStars / totalRatings);
+  }
+
+  useEffect(() => {
+    findAvgStars();
+
+  }, [])
 
   return (
     <div>
-      Ratings
+      <section>
+        Total Stars: {props.avgStars}
+      </section>
+      <section>
+        Stars
+      </section>
+      <section>
+        Recommended: {props.sampleMetaData.recommend}
+      </section>
+      <section> Total stars:
+        <div>1: {starsObj['1'] || 0}</div>
+        <div>2: {starsObj['2'] || 0}</div>
+        <div>3: {starsObj['3'] || 0}</div>
+        <div>4: {starsObj['4'] || 0}</div>
+        <div>5: {starsObj['5'] || 0}</div>
+      </section>
+      <section>
+        characteristics:
+        {Object.keys(props.sampleMetaData.characteristics).map((charKey, index) => {
+          return(<CharacteristicsCreator charKey={charKey} obj={props.sampleMetaData.characteristics[charKey]} key={index}/>)
+        })}
+
+      </section>
     </div>
     );
 }
