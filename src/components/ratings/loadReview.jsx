8cb7458f89abddcc566864a112b5c1c18fdb973a
Halley Bennett <halley.bennett@gmail.com>
2022-10-14 15:57:44 -0600

    Completed Reviews Skeleton

--- src/components/ratings/loadReview.jsx
+++ src/components/ratings/loadReview.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const LoadReview = () => {
-  return (<div>Each Review</div>);
+const LoadReview = (props) => {
+  return (
+  <div>
+    Each Review Stars:
+    {props.review.rating}
+    Username: {props.review.reviewer_name}
+    Date: {props.review.date}
+    <div>
+      Recommended: {props.review.recommend}
+    </div>
+    <div>
+      Title: {props.review.summary}
+    </div>
+    <div>
+      Body: {props.review.body}
+    </div>
+    <div>
+      Helpfulness: {props.review.helpfulness}
+    </div>
+    <div>
+      Response: {props.review.response}
+    </div>
+  </div>
+  );
 }
 
 export default LoadReview;
