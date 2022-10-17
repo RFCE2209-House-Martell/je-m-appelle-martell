a68488794634eabbc80858d941892d3250053844
fitdizzle <markfitnessaz@gmail.com>
2022-10-17 08:29:24 -0700

    deleted qa component from index and renamed feature folder / component Questions

---
+++ src/components/questions/newQuestionModal.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const NewQuestionModal = (props) => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default NewQuestionModal;
--- src/components/questions/questionList.jsx
+++ src/components/questions/questionList.jsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import API from './functions/api.js';
+import axios from 'axios';
+
 import Question from './question.jsx';
+import API from './api.js';
 
 const QuestionList = (props) => {
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    const loadQuestions = () => {
-      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/questions/:${props.id}`).then((res) => {
-        setQuestions(res.data);
-      })
+    const loadQuestions = async () => {
+
+      const headers = {
+        Authorization: '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',
+      };
+
+      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/qa/questions?product_id=66642`, { headers: headers }).then((res) => {
+        setQuestions(res.data.results);
+      });
+
     };
 
     loadQuestions();
   }, []);
 
-  return (<div>{questions.map((question) => {
-    return (<Question question={question} />)
-  })}</div>);
+  return (<div>{questions.length > 0 ? questions.map((question) => {
+    return (<Question key={question.question_id} question={question} />)
+  }) : null}</div>);
 }
 
 export default QuestionList;
