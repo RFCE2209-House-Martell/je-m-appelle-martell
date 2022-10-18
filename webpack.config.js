054592227fb34797d1ac5efce85247c48d9b7d64
fitdizzle <markfitnessaz@gmail.com>
2022-10-18 10:43:02 -0700

    updated webpack config to handle front end env files. also installed dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -7,6 +8,11 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: "bundle.js"
   },
+  plugins: [
+    new Dotenv({
+      systemvars: true,
+    }),
+  ],
   module: {
     rules: [
       {
@@ -16,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader']
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
