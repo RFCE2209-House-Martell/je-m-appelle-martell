af89ea09d9706d075fb86e1b88a7c89f1f8209b7
Halley Bennett <halley.bennett@gmail.com>
2022-10-18 12:44:19 -0600

    Made progress on making the page look good

--- src/components/ratings/reviewsSection.jsx
+++ src/components/ratings/reviewsSection.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoadReview from './loadReview.jsx';
 
 const ReviewsSection = (props) => {
+  const data = props.reviewData.results;
   const [moreResults, setMoreResults] = useState(false);
+
   if (Object.keys(props.reviewData).length < 1) {
     return (
       <div>
@@ -10,10 +12,22 @@ const ReviewsSection = (props) => {
       </div>
     )
   }
-  const data = props.reviewData.results;
+
+  const showTempValues = () => {
+    console.log('yes')
+    let times = 0;
+    return data.map((review, index) => {
+      if (review.rating === props.reviewStar && times < 2) {
+        times++;
+        return(<LoadReview review={review} key={index}/>)
+      }
+    })
+  }
+
   return (
   <div>
     Reviews count: {data.length}
+    <div class='reviewScroll'>
     {(moreResults) ? (
       data.map((review, index) => {
         if (props.reviewStar === 0) {
@@ -24,11 +38,15 @@ const ReviewsSection = (props) => {
           }
         }
       })
-    ) : <div>
+    ) : (props.reviewStar === 0) ? (<div>
       <LoadReview review={data[0]}/>
       <LoadReview review={data[1]}/>
-      </div>
+      </div>) : showTempValues()
     }
+    </div>
+    <div>
+      <button onClick={e => setMoreResults(!moreResults)}>More Results</button>
+    </div>
   </div>);
 }
 
