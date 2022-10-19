f7e227b63b641b939706e4514a1ba55e5cafbdc7
czhu67 <caitlin.zhu2010@gmail.com>
2022-10-19 16:15:03 -0700

    made some changes to zoom box logic

--- src/components/overview/expandedView.jsx
+++ src/components/overview/expandedView.jsx
@@ -3,32 +3,26 @@ import React, { useState, useEffect} from 'react';
 const ExpandedView = (props) => {
   const [zoomIn, setZoomIn] = useState(false);
   const [mousePos, setMousePos] = useState({x: 0, y: 0});
+  const [overImg, setOverImg] = useState(true);
 
-  // useEffect(() => {
-
-  // }, []);
-
-  var handleZoom = (e) => {
-    setMousePos({x: e.clientX, y: e.clientY});
-    window.addEventListener('mousemove', (event) => setMousePos({x: event.clientX, y: event.clientY}))
-  };
+  var handleMouseMove = (e) => {
+    setMousePos({x: e.clientX, y: e.clientY}); // position of mouse move
+    setOverImg(event.target.className.includes('zoomImage')); // sets overImg as false when mouse is not over image
+  }
 
   return (
-    <div className="zoomView" onClick={(e) => {
+    <div className="zoomView" onMouseMove={(e) => handleMouseMove(e)} onClick={(e) => {
       if (e.target.className === "zoomView") {
         props.setZoom(false);
       }
     }}>
       {props.selected === 0 ? null: <button className="zoomarrow" onClick={() => props.handleArrows('left')}>{'<'}</button>}
       <div className="zoomBorder">
-        <img src={props.img} className="zoomImage" onClick={(e) => {
+        <img src={props.img} className={`zoomImage zoomImage-${zoomIn}`} onClick={(e) => {
           setZoomIn(!zoomIn);
-          if (!zoomIn) {
-            handleZoom(e);
-          }
         }}/>
         <div className="closeZoom" onClick={() => {props.setZoom(false)}}>𐄂</div>
-        {zoomIn ? <div className="movingZoom" style={{left: mousePos.x + 10, top: mousePos.y + 10}}>testing!</div> : null}
+        {zoomIn && overImg ? <div className="movingZoom" style={{left: mousePos.x + 10, top: mousePos.y + 10}}>testing!</div> : null}
       </div>
       {props.selected === props.photos.length - 1 ? null : <button className="zoomarrow" onClick={() => props.handleArrows('right')}>{'>'}</button>}
     </div>
