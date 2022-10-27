346787685db40d1a61568841cdfde766f874201b
fitdizzle <markfitnessaz@gmail.com>
2022-10-27 08:54:21 -0700

    adding logger to index

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,8 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 import axios from 'axios';
 import Modal from './components/sharedFolder/modal.jsx';
+import Logger from './components/logger/logger.jsx';
+import loggerAPI from './components/logger/api.js';
 
 const App = () => {
   const [productId, setProductId] = useState(66642);
@@ -32,35 +34,35 @@ const App = () => {
   }, []);
 
   const changeRelatedProductFeatures = (id) => {
-    axios.get (`${process.env.REACT_APP_BASE_URL}products/${id}`, {
+    axios.get(`${process.env.REACT_APP_BASE_URL}products/${id}`, {
       headers: {
         'Authorization': process.env.REACT_APP_API_KEY
       },
     })
-    .then(data => {
-      console.log('DATA', data)
-      setRelatedProductName(data.data.name)
-      setRelatedProductFeatures(data.data.features)
-      getOverviewFeatures(productId)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then(data => {
+        console.log('DATA', data)
+        setRelatedProductName(data.data.name)
+        setRelatedProductFeatures(data.data.features)
+        getOverviewFeatures(productId)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   const getOverviewFeatures = (id) => {
-    axios.get (`${process.env.REACT_APP_BASE_URL}products/${id}`, {
+    axios.get(`${process.env.REACT_APP_BASE_URL}products/${id}`, {
       headers: {
         'Authorization': process.env.REACT_APP_API_KEY
       },
     })
-    .then(data => {
-      setOverviewProductName(data.data.name)
-      setOverviewProductFeatures(data.data.features)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then(data => {
+        setOverviewProductName(data.data.name)
+        setOverviewProductFeatures(data.data.features)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   const traits = {};
@@ -80,34 +82,43 @@ const App = () => {
   })
 
   const compareModalStyle = {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: 'rgba(255, 255, 255, 0.75)',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 
   var showCart = () => {
     console.log(cart);
   };
 
+  const handleElementClick = (e) => {
+    if (e.target.id) {
+      return loggerAPI.logUserInteraction(e).then((res) => {
+        console.log('Result from logger' + res);
+      }).catch((err) => console.log('Err in logger' + err));
+    };
+  }
+
   return (
     <>
-      <div>
-        <Modal className='compare' show={showCompare} onClose={() => setShowCompare(false)} styles={compareModalStyle} >
-          <button onClick={() => {setShowCompare(false)}}>X</button>
-            <h3 style={{justifyContent: 'center', display: 'flex'}}>Comparision</h3>
-          <table>
-            <tr>
-              <th>{overviewProductName}</th>
-              <th>Feature</th>
-              <th>{relatedProductName}</th>
-            </tr>
+      <Logger logHandler={(e) => handleElementClick(e)}>
+        <div>
+          <Modal className='compare' show={showCompare} onClose={() => setShowCompare(false)} styles={compareModalStyle} >
+            <button onClick={() => { setShowCompare(false) }}>X</button>
+            <h3 style={{ justifyContent: 'center', display: 'flex' }}>Comparision</h3>
+            <table>
+              <tr>
+                <th>{overviewProductName}</th>
+                <th>Feature</th>
+                <th>{relatedProductName}</th>
+              </tr>
               {Object.keys(traits).map((trait) => {
                 return (
                   <tr>
@@ -117,26 +128,27 @@ const App = () => {
                   </tr>
                 )
               })}
-          </table>
-        </Modal>
-        <div className="header">
-          <img className="logo" src={require('./components/sharedFolder/martell-logo.png').default} alt="martell-logo" />
-          <div className="cart" onClick={showCart}>
-            <div className="cartCount">{cart.length}</div>
-            <IconContext.Provider value={{size: '40px'}}>
-              <AiOutlineShoppingCart className="cartIcon"/>
-            </IconContext.Provider>
+            </table>
+          </Modal>
+          <div className="header">
+            <img className="logo" src={require('./components/sharedFolder/martell-logo.png').default} alt="martell-logo" />
+            <div className="cart" onClick={showCart}>
+              <div className="cartCount">{cart.length}</div>
+              <IconContext.Provider value={{ size: '40px' }}>
+                <AiOutlineShoppingCart className="cartIcon" />
+              </IconContext.Provider>
+            </div>
+          </div>
+          <div className="app">
+            <Overview productId={productId} avgStars={avgStars} />
+            <RelatedApp changeRelatedProductFeatures={changeRelatedProductFeatures} setShowCompare={setShowCompare} productId={productId} setProductId={setProductId} outfitProducts={outfitProducts} setOutfitProducts={setOutfitProducts} />
+            <QuestionsAnswersContainer productId={productId} />
+            <Ratings productId={productId} avgStars={avgStars} setAvgStars={setAvgStars} />
           </div>
         </div>
-        <div className="app">
-          <Overview productId={productId} avgStars={avgStars} />
-          <RelatedApp changeRelatedProductFeatures={changeRelatedProductFeatures} setShowCompare={setShowCompare} productId={productId} setProductId={setProductId} outfitProducts={outfitProducts} setOutfitProducts={setOutfitProducts} />
-          <QuestionsAnswersContainer productId={productId} />
-          <Ratings productId={productId} avgStars={avgStars} setAvgStars={setAvgStars} />
-        </div>
-      </div>
+      </Logger>
     </>
-);
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
