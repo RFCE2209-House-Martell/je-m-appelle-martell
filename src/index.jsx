0db25cdf3785bbac34d003a80a799b6fb16737ef
czhu67 <caitlin.zhu2010@gmail.com>
2022-10-28 09:12:46 -0700

    some stuff

--- src/index.jsx
+++ src/index.jsx
@@ -16,7 +16,7 @@ import Logger from './components/logger/logger.jsx';
 import loggerAPI from './components/logger/api.js';
 
 const App = () => {
-  const [productId, setProductId] = useState(66642);
+  const [productId, setProductId] = useState(66658);
   const [avgStars, setAvgStars] = useState(0);
   const [outfitProducts, setOutfitProducts] = useState([]);
   const [cart, setCart] = useState([]);
